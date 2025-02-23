3194d2588b50851774d4e127c0fd4e9d7289cea4
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-23 11:20:35 +0200

    Added a new example file from the book.

---
+++ 13.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+if __name__ = "__main__":
+    print("Do something")
+    
+import intertools
+intertools.count()
+
+import intertools as it
+it.count()
+
+import math as m
+a = m.pi * 10
+
+from math import pi
+a = pi * 10
+
+from math import (pi, floor, sin, cos)
+
+from imp import reload
+reload(module_name)
