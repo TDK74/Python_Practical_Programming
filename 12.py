a1f65c7166d7d5a78174318e5e2c7d66446fe17d
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-23 11:19:33 +0200

    Added a new example file from the book.

---
+++ 12.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+time.time()
+
+t = time.time()
+time.gmtime(t)
+
+tm = time.gmtime(t)
+tm.tm_hour
+
+time.localtime()
+
+time.strftime("%d.%m.%y")
+time.strftime("%d.%m.%Y")
+time.strftime("%H:%M:%S")
+
+import locale
+locale.getlocale()
+locale.setlocale(locale.LC_ALL, ('bulgarian'))
+print(time.strftime("%A %d %b %Y %H:%M:%S \n%d.%m.%Y"))
+
+import calendar
+c = calendar.LocaleTextCalendar(0, "Bulgarian_Bulgaria.1251")
+print(c.formatyear(2018))
+
+import time as t
+t.sleep(10)
+
+import timeit
+t = timeit.Timer('char in text', setup = 'text = "sample string"; char = "g"')
+t.timeit()
+t.repeat()
+
+from time import *
+t1 = time()
+# Код, чието време за изпълнение трябва да измерим
+print('Hello')
+t2 = time()
+t = t2 - t1
