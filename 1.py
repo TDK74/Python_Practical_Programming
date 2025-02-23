31107e3a3ab5e0ba1780e3ae0e6e76e1638d57ee
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-23 11:03:09 +0200

    Added a new example file from the book.

---
+++ 1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*import
+
+# this is comment
+print('Hi') # another comment
+
+print('# comment')
+
+# more than one line
+# comment
+
+"""
+multiline
+comment
+"""
+
+a = 1; b = 2;
+print(a, b)
+print(a, b, sep = '\t')
+
+print()
+
+import sys;
+sys.stdout.write("hello")
+sys.stdout.write("hello\n")
+
+name = input("what is your name? ")
+print("hello, ", name)
+
+name = input("what is your name?\n")
+
+try:
+  name = input("what is your name? ")
+  print(name)
+except EOFError:
+  print("EOFError raised")
+  
+import sys
+args = sys.argv[:]
+for n in args:
+  print(n)
