adab0f6bf0af6254d461c2bdd02ecfd5c23cb117
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-23 11:40:55 +0200

    Update 21_1.py
    Minor corrections

--- 21_1.py
+++ 21_1.py
@@ -5,36 +5,32 @@ import sys
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import *
 
-# syzdavame prilojenie i prozorec
+
 app = QApplication(sys.argv)
 
 w = QWidget()
 w.setWindowTitle('Sample')
 
-# buton
+
 btn = QPushButton('Click me', w)
 
 @pyqtSlot()
 def on_click():
-    ''' izvikva se pri klikvane s butona.'''
     print('clicked')
     
 @pyqtSlot()
 def on_press():
-    ''' izvikva se pri natisnat butona.'''
     print('pressed')    
 
 @pyqtSlot()
 def on_release():
-    ''' izvikva se, kogato butona se otpusne.'''
     print('released')
     
-# saediniavane na signalite s tehnite slotove
+
 btn.clicked.connect(on_click)
 btn.pressed.connect(on_press)
 btn.released.connect(on_release)
 
-# pokazvame prozoreca i startirame prilojenieto
+
 w.show()
 app.exec_()
-
